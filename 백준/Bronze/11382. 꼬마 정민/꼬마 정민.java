1d8fd114af33d3ff6157a64074790c999b6a90bd
yujin <84698468+yudii2@users.noreply.github.com>
2025-07-08 11:53:45 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        final long a = scanner.nextLong();
+        final long b = scanner.nextLong();
+        final long c = scanner.nextLong();
+        scanner.close();
+
+        System.out.println(a + b + c);
+    }
+}
+
